4bf0dcd7973c52e62435c324b93286b6c92dd036
Daniel Rubio <drubio1989@gmail.com>
2023-08-16 14:21:24 -0500

    Implement grid layout for dashboards

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { themeSettings } from "./theme"
 import { CssBaseline } from "@mui/material";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -17,7 +18,7 @@ function App() {
             <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
               <Navbar />
                 <Routes>
-                  <Route path="/" element={<div>dashboard page</div>}></Route>
+                  <Route path="/" element={<Dashboard />}></Route>
                   <Route path="/predictions" element={<div>predictions page</div>}></Route>
                 </Routes>
             
